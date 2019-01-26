88a24e86d531c40f0adfa036f39bbe762d4b7d76
Aniol Marti <aniol@aniolmarti.cat>
2019-01-26 21:57:07 +0100

    Arreglo bug amb el logout

--- contrasenyes-usuaris/logout.php
+++ contrasenyes-usuaris/logout.php
@@ -1,6 +1,8 @@
 <?php
-
-unset($_SESSION);
+session_unset();
+session_destroy();
+session_write_close();
+setcookie(session_name(),'',0,'/');
+session_regenerate_id(true);
 header('Location: login');
-
 ?>
